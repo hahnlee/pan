a71d1c5518d6fd18bf3bbd66defbb24a4790b29d
hahnlee <hanlee.dev@gmail.com>
2022-09-07 14:09:49 +0900

    feat(hermes): binding compileJS

--- hermes/binding/binding.cc
+++ hermes/binding/binding.cc
@@ -1,11 +1,26 @@
 #include <hermes/hermes.h>
+#include <hermes/CompileJS.h>
 #include <jsi/jsi.h>
 
-using namespace facebook;
 using namespace facebook::hermes;
 
 extern "C"
 {
+  bool hermes__compileJS(const char *str, const char *&data, size_t &size, bool optimize)
+  {
+    std::string code = std::string(str);
+    std::string bytecode;
+
+    bool result = hermes::compileJS(code, bytecode, optimize);
+    if (result)
+    {
+      data = bytecode.data();
+      size = bytecode.size();
+    }
+
+    return result;
+  }
+
   bool hermes__isHermesBytecode(const uint8_t *data, size_t len)
   {
     return HermesRuntime::isHermesBytecode(data, len);
--- hermes/src/lib.rs
+++ hermes/src/lib.rs
@@ -3,8 +3,36 @@ pub mod jsi;
 pub mod runtime;
 pub mod support;
 
+use std::ffi::CString;
+
+extern "C" {
+    fn hermes__compileJS(
+        code: *const i8,
+        data: &*mut u8,
+        size: *mut libc::size_t,
+        optimize: u8,
+    ) -> u8;
+}
+
+pub fn compile_js(code: &str, optimize: bool) -> Result<&[u8], ()> {
+    let bytecode = std::ptr::null_mut::<u8>();
+    let mut size: usize = 0;
+
+    let code = CString::new(code).unwrap();
+
+    let result = unsafe { hermes__compileJS(code.as_ptr(), &bytecode, &mut size, optimize as u8) };
+    if result == 0 {
+        return Err(());
+    }
+
+    let slice = unsafe { std::slice::from_raw_parts(bytecode, size) };
+
+    return Ok(slice);
+}
+
 #[cfg(test)]
 mod tests {
+    use crate::compile_js;
     use crate::runtime;
 
     #[test]
@@ -19,7 +47,25 @@ mod tests {
     }
 
     #[test]
+    fn check_compile_js() {
+        let valid = compile_js("x = 1", false);
+        assert_eq!(valid.is_ok(), true);
+    }
+
+    #[test]
+    fn check_compile_js_err() {
+        let invalid = compile_js("fn test() {}", false);
+        assert_eq!(invalid.is_err(), true);
+    }
+
+    #[test]
     fn check_bytecode() {
+        let valid = compile_js("x + 2", false).unwrap();
+        assert_eq!(runtime::HermesRuntime::is_hermes_bytecode(&valid), true)
+    }
+
+    #[test]
+    fn check_bytecode_err() {
         let invalid: [u8; 0] = [];
         assert_eq!(runtime::HermesRuntime::is_hermes_bytecode(&invalid), false);
     }
