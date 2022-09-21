508d5621ab0f504340cb090025a1349bfa04fee5
hahnlee <hanlee.dev@gmail.com>
2022-09-21 11:07:15 +0900

    feat(pan): run from file

--- crates/pan/src/lib.rs
+++ crates/pan/src/lib.rs
@@ -1,1 +1,22 @@
+use std::fs;
+
+use hermes::buffer::MemoryBuffer;
+use hermes::jsi::runtime::Runtime;
+use hermes::runtime::HermesRuntime;
+use hermes::runtime::OwnedHermesRuntime;
+
 pub mod version;
+
+// TODO: (@hahnlee) provide FFI function
+pub fn initialize() -> OwnedHermesRuntime {
+    OwnedHermesRuntime::new()
+}
+
+// TODO: (@hahnlee) provide error code for FFI
+pub fn run_js(runtime: &HermesRuntime, file_path: &str) {
+    let file = fs::read(file_path).unwrap();
+    let buffer = MemoryBuffer::from_bytes(&file);
+
+    // FIXME: (@hahnlee) convert file_path to url
+    runtime.evaluate_javascript(&buffer, file_path);
+}
