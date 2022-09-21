323db99bcb5bb06c4dab41c86053889b4a42857a
hahnlee <hanlee.dev@gmail.com>
2022-09-21 15:22:49 +0900

    feat(cli): apply new runtime

--- crates/cli/src/main.rs
+++ crates/cli/src/main.rs
@@ -7,6 +7,6 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     let file_path = &args[1];
 
-    let runtime = initialize();
-    run_js(&runtime, file_path);
+    let mut runtime = initialize();
+    run_js(&mut runtime, file_path);
 }
